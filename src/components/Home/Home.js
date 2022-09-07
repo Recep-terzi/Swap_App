9e005815b358df167d9022900dccf9d30930d61b
Recep Terzi <recepterzi-67@hotmail.com>
2022-09-07 14:25:59 +0300

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Items from "../Items/Items";
 import "./Home.Module.css";
+import clothes from "../../assets/clothes.png";
+import swap from "../../assets/swap.png";
 const Home = () => {
   return (
     <>
@@ -8,7 +10,51 @@ const Home = () => {
         <p>Welcome to Swap App!</p>
       </div>
       <div className="home-container">
-        <Items />
+        <div className="row home-row">
+          <div className="col-md-7">
+            <div className="home-title">Clothing Swap</div>
+            <div className="home-description">Give your clothes a new life</div>
+            <div className="home-description-other">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+              reprehenderit blanditiis, porro quae quidem, iusto animi
+              aspernatur quam accusantium fugiat molestias quia possimus
+              cupiditate recusandae voluptate voluptas tempora quaerat expedita!
+              Similique minus neque, omnis architecto, in rerum laudantium quam
+              porro inventore exercitationem illum ipsa tempore molestiae
+              nesciunt pariatur! Ad, debitis.
+            </div>
+            <div className="home-btn">
+              <button>Daha Fazla</button>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <img src={clothes} alt="clothes-img"></img>
+          </div>
+        </div>{" "}
+        <div className="row home-row">
+          <div className="col-md-6">
+            <img src={swap} alt="clothes-img"></img>
+          </div>
+          <div className="col-md-6">
+            <div className="home-title">Item Swap</div>
+            <div className="home-description">Give your items a new life</div>
+            <div className="home-description-other">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+              reprehenderit blanditiis, porro quae quidem, iusto animi
+              aspernatur quam accusantium fugiat molestias quia possimus
+              cupiditate recusandae voluptate voluptas tempora quaerat expedita!
+              Similique minus neque, omnis architecto, in rerum laudantium quam
+              porro inventore exercitationem illum ipsa tempore molestiae
+              nesciunt pariatur! Ad, debitis.
+            </div>
+            <div className="home-btn">
+              <button>Daha Fazla</button>
+            </div>
+          </div>
+        </div>
+        <div className="items-container">
+          <Items />
+        </div>
       </div>
     </>
   );
--- src/components/ItemsDetail/ItemsDetail.js
+++ src/components/ItemsDetail/ItemsDetail.js
@@ -6,6 +6,7 @@ import { db } from "../../firebase/config";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../../redux/swapSlice";
 import { Carousel } from "react-responsive-carousel";
+import ReactStarsRating from "react-awesome-stars-rating";
 import notebook from "../../assets/notebook.jpg";
 const ItemsDetail = () => {
   const { id } = useParams();
@@ -38,7 +39,8 @@ const ItemsDetail = () => {
                   </div>
                 ))}{" "} */}
                 {/* <ImageGallery items={info.image} /> */}
-                {/* <CarouselProvider
+                {
+                  /* <CarouselProvider
                   naturalSlideWidth={100}
                   naturalSlideHeight={125}
                   totalSlides={3}
@@ -52,7 +54,50 @@ const ItemsDetail = () => {
                   </Slider>
                   <ButtonBack>Back</ButtonBack>
                   <ButtonNext>Next</ButtonNext>
-                </CarouselProvider> */}
+                </CarouselProvider> */
+                  // info.image.map((img, index) => (
+                  // ))
+                }
+                <img
+                  src={notebook}
+                  alt="notebook"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "50px",
+                    padding: "40px",
+                  }}
+                ></img>
+              </div>
+              <div className="col-md-6">
+                <div className="info-div">
+                  <div className="info-title">{info.title}</div>
+                  <div className="info-description">{info.description}</div>
+                  <div className="info-star">
+                    <ReactStarsRating
+                      value={info.star}
+                      isEdit={false}
+                      size={15}
+                    />{" "}
+                    <span>{info.star}</span>
+                  </div>
+                  <div className="info-price">
+                    {info.price} <span>TL</span>
+                  </div>
+                  <div className="info-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Minima esse quisquam doloremque eos ipsam. Explicabo itaque
+                    tempore in voluptates magnam quam minima est suscipit quasi
+                    quis debitis quas earum, nesciunt incidunt eveniet. Dolorem
+                    ratione aspernatur in harum? Delectus, ducimus? Veniam velit
+                    sed perspiciatis delectus laborum repellat cupiditate,
+                    impedit in doloribus!
+                  </div>
+                  <div className="info-button">
+                    <button className="teklifBtn"> Teklif Ver </button>
+                    <button className="satinAlBtn"> Satın Al </button>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
