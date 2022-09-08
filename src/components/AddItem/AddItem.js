63ee92f41aaff2b0ae88d482e06dcc0ac823d063
Recep Terzi <recepterzi-67@hotmail.com>
2022-09-08 20:07:34 +0300

    Update AddItem.js

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,47 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddItem.Module.css";
+import { db } from "../../firebase/config";
+import { addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
 const AddItem = () => {
+  const [title, setTitle] = useState();
+  const [price, setPrice] = useState();
+  const [star, setStar] = useState();
+  const [image, setImage] = useState();
+  const [image2, setImage2] = useState();
+  const [image3, setImage3] = useState();
+  const [description, setDescription] = useState();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const doc = {
+      title: title,
+      price: price,
+      image: image,
+      image2: image2,
+      image3: image3,
+      star: star,
+      description: description,
+    };
+    const ref = collection(db, "items");
+    try {
+      await addDoc(ref, {
+        ...doc,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="additem-div">
-        <div className="additem-title">Welcome to, add item page!</div>
-        <div className="additem-item-title">
-          <span>İtem Title</span>
-          <input type="text" />
-        </div>
-        <div className="additem-item-title">
-          <span>İtem Price</span>
-          <input type="text" />
-        </div>
-        <div className="additem-item-title">
-          <span>İtem Star</span>
-          <input type="number" max={5} min={0} />
-        </div>
-        <div className="additem-item-image">
-          <span>İtem İmage</span>
-          <div className="row">
-            <div className="col-md-4">
-              <span>Resim 1</span>
-              <input type="text" />
-            </div>
-            <div className="col-md-4">
-              <span>Resim 2</span>
-              <input type="text" />
-            </div>
-            <div className="col-md-4">
-              <span>Resim 3</span>
-              <input type="text" />
+        <form onSubmit={handleSubmit}>
+          <div className="additem-title">Welcome to, add item page!</div>
+          <div className="additem-item-title">
+            <span>İtem Title</span>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="additem-item-title">
+            <span>İtem Price</span>
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+          <div className="additem-item-title">
+            <span>İtem Star</span>
+            <input
+              type="number"
+              max={5}
+              min={0}
+              value={star}
+              onChange={(e) => setStar(e.target.value)}
+            />
+          </div>
+          <div className="additem-item-image">
+            <span>İtem İmage</span>
+            <div className="row">
+              <div className="col-md-4">
+                <span>Resim 1</span>
+                <input
+                  type="text"
+                  value={image}
+                  onChange={(e) => setImage(e.target.value)}
+                />
+              </div>
+              <div className="col-md-4">
+                <span>Resim 2</span>
+                <input
+                  type="text"
+                  value={image2}
+                  onChange={(e) => setImage2(e.target.value)}
+                />
+              </div>
+              <div className="col-md-4">
+                <span>Resim 3</span>
+                <input
+                  type="text"
+                  value={image3}
+                  onChange={(e) => setImage3(e.target.value)}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="additem-item-title">
-          <span>İtem Description</span>
-          <textarea name="" id="" cols="30" rows="5"></textarea>
-        </div>
-        <div className="additem-button">
-          <button type="submit"> İlana koy! </button>
-        </div>
+          <div className="additem-item-title">
+            <span>İtem Description</span>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="5"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="additem-button">
+            <button type="submit"> İlana koy! </button>
+          </div>
+        </form>
       </div>
     </>
   );
