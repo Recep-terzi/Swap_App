401de682d885a94175ebac0723e02debb07109de
Recep Terzi <recepterzi-67@hotmail.com>
2022-09-15 15:56:50 +0300

    update

--- src/components/UpdateItem/UpdateItem.js
+++ src/components/UpdateItem/UpdateItem.js
@@ -1,68 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import "./UpdateItem.Module.css";
-import { FormControl, InputLabel, MenuItem, TextField } from "@mui/material";
-import Box from "@mui/material/Box";
-import Select from "@mui/material/Select";
-
+import { Link, useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { useDispatch, useSelector } from "react-redux";
+import { getDetail } from "../../redux/swapSlice";
 const UpdateItem = () => {
   const categories = ["Kıyafet", "Teknolojik Ürünler", "Diğer"];
-  const [category, setCategory] = useState();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  //   const [loading, setLoading] = useState(true);
+  const detail = useSelector((state) => state.swap.detail);
+  console.log(detail);
+  useEffect(() => {
+    const ref = doc(db, "items", id);
+    const data = [];
+    getDoc(ref).then((snap) => {
+      if (snap.exists) {
+        data.push(snap.data());
+      } else {
+        console.log("error");
+      }
+      dispatch(getDetail(data));
+    });
+  }, [dispatch, id]);
+  //   useEffect(() => {
+  //     setTimeout(() => {
+  //       setLoading(false);
+  //     }, 2000);
+  //   }, []);
 
   return (
     <>
       <Navbar />
-
       <div className="updateitem-container">
         <div className="update-item-text">
           Ürününüzü güncellemek mi istiyorsunuz? Aşağıdan düzenlemeyi
           yapabilirsiniz. Bol kazançlar. İyi takaslar.
         </div>
+
         <form>
           <div className="form-card">
+            <div className="update-item-img">
+              <img src={detail[0].image} alt="" />
+              <img src={detail[0].image2} alt="" />
+              <img src={detail[0].image3} alt="" />
+            </div>
             <div className="title-input">
               <label>Ürün Adı </label>
-              <input type="text" />
+              <input type="text" value={detail[0].title} />
             </div>
             <div className="price-input">
               <label>Ürün Fiyatı </label>
 
-              <input type="text" />
+              <input type="text" value={detail[0].price} />
             </div>
             <div className="category-selection">
-              <Box>
-                <FormControl fullWidth sx={{ mt: 3 }}>
-                  <InputLabel id="demo-simple-select-label">
-                    Kategori Seçiniz
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Kategori Seçiniz"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                  >
-                    {categories.map((category, index) => (
-                      <MenuItem value={category} key={index}>
-                        {category}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Box>
+              <label> Ürün Kategorisi </label>
+              <select value={detail[0].category}>
+                {categories.map((category) => (
+                  <option>{category}</option>
+                ))}
+              </select>
             </div>
             <div className="description-input">
-              <TextField
-                margin="dense"
-                id="outlined-multiline-static"
-                label="Ürünün Açıklaması"
-                type="text"
-                fullWidth
-                inputProps={{ maxLength: "28" }}
-                variant="standard"
-                rows={4}
-              />
+              <label>Ürün Açıklaması</label>
+              <textarea value={detail[0].description}></textarea>
+            </div>
+            <div className="update-item-button">
+              <button>Güncelle</button>
             </div>
           </div>
         </form>
