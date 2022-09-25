201df748193468c2324251ac65f155898e68164d
Recep Terzi <recepterzi-67@hotmail.com>
2022-09-25 21:39:13 +0300

    update

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -5,19 +5,14 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase/config";
 import { useSelector } from "react-redux";
 const Comment = () => {
-  const [comment, setComment] = useState([]);
-  const [comment2, setComment2] = useState([]);
-  const [commentTitle, setCommentTitle] = useState([]);
   const { id } = useParams();
   const [newComment, setNewComment] = useState([]);
   const detail = useSelector((state) => state.swap.detail);
   const handleSubmit = (e) => {
     e.preventDefault();
-    setComment([...comment, newComment]);
-    setComment2([...comment2, commentTitle]);
     const ref = doc(db, "items", id);
     updateDoc(ref, {
-      comment: [...comment, newComment, ...comment2, commentTitle],
+      comment: [...detail[0].comment, newComment],
     })
       .then((res) => {
         console.log("success");
@@ -36,34 +31,23 @@ const Comment = () => {
               {detail[0].comment.map((det) => (
                 <>
                   <div className="comment-div">
-                    <div className="title-time-div">
-                      <div className="comment-title">{det}</div>
+                    <div className="author-time-div">
+                      <div className="comment-author">Pyson</div>
+
                       <div className="comment-time">25.09.2022</div>
                     </div>
-                    <div className="comment-author">Pyson</div>
-                    <div className="comment-description">
-                      Satıcı anında teklifime dönüş yaptı. Ve üründen de çok
-                      memnunum. Teşekkür ederim ilginiz için. Umarım daha sonra
-                      tekrar görüşürüz.
-                    </div>
+                    <div className="comment-description">{det}</div>
                   </div>
                 </>
               ))}
             </>
           ) : (
-            <h1>Yok</h1>
+            <h1>Yorum yok</h1>
           )}
 
           <div className="input-button-div">
             <h2>Haydi, sende yorum yap!</h2>
             <div className="input-div">
-              <input
-                type="text"
-                placeholder="Yorum başlığı"
-                value={commentTitle}
-                className="comment--title"
-                onChange={(e) => setCommentTitle(e.target.value)}
-              />
               <textarea
                 type="text"
                 placeholder="Yorum Açıklaması"
